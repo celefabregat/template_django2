8afb65613fbecac3084b0f0702fb9b03e8384db6
JulianBenitezM <julianbenitezmerlo@gmail.com>
2023-07-23 10:20:14 -0300

    estructura

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject2 project.
+URL configuration for blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', admin.site.urls),
-
+    path('', index, name='index'),
 ]
