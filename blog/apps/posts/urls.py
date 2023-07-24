96bd7af92be2a180d5a4f0ca122bd8e9231d23fe
JulianBenitezM <julianbenitezmerlo@gmail.com>
2023-07-24 18:26:36 -0300

    material complementario 3.2 pag10

--- blog/apps/posts/urls.py
+++ blog/apps/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PostListView,PostDetailView
 from . import views
 
-
+app_name = 'apps.posts'
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='posts'),
