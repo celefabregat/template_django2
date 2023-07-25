f9d4ae433750fe8afbdf7016e79d0120d0658ca1
JulianBenitezM <julianbenitezmerlo@gmail.com>
2023-07-25 12:36:07 -0300

    salto de fe

--- blog/blog/configuraciones/local.py
+++ blog/blog/configuraciones/local.py
@@ -1,1 +1,17 @@
-from .settings import *
+from .base import *
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "blog",
+        "USER": "julian",
+        "PASSWORD": "julianbenitez1",
+        "HOST": "localhost",
+        "PORT": "3306",
+    }
+}
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+#request 'es un diccionario que continuamente se va pasando entre el navegador y el servidor'
 
-def index(request):
-    return render(request, 'index.html')
+def Home(request):
 
+	return render(request, 't_home.html')
+
+
+def nosotros(request):
+
+	return render(request, 't_nosotros.html')
